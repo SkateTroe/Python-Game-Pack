5ab06e9baefe401893ac49ae845232a41e5ed502
SkateTroe <33695193+SkateTroe@users.noreply.github.com>
2017-11-20 17:19:32 -0600

    Add files via upload
    Game setup for pvp up and running. Working out game play now.

--- Battleship.py
+++ Battleship.py
@@ -1,3 +1,9 @@
+import os
+
+cShipYard = [['A','B','C','D','P'],
+			[5,4,3,3,2],
+			['Aircraft Carrier','Battleship','Cruiser','Destroyer', 'Patrol Ship']]
+
 def BlankMap():
 	Map = [['  ','1 ','2 ','3 ','4 ','5 ','6 ','7 ','8 ','9 ','0 '],
 		   ['A ','- ','- ','- ','- ','- ','- ','- ','- ','- ','- '],
@@ -13,9 +19,6 @@ def BlankMap():
 	return Map
 
 def FillBoard():
-	ShipYard = [['A','B','C','D','P'],
-				[5,4,3,3,2],
-				['Aircraft Carrier','Battleship','Cruiser','Destroyer', 'Patrol Ship']]
 
 	Map = BlankMap()
 	
@@ -25,33 +28,35 @@ def FillBoard():
 		while go == 0:
 			Coordinates = [0]
 			while Coordinates[0] == 0:
-				print("Please select starting position for your ", ShipYard[2][i])
+				print("Please select starting position for your ", cShipYard[2][i])
 				go2 = 0
 				while go2 == 0:
 					StartPoint = input()
 					Coordinates = ConvertCoordinates(StartPoint)
 					row = int(Coordinates[0])
 					column = int(Coordinates[1])
+					if column == 0:
+						column = 10
 					if Map[row][column] != '- ':
-						print("You already have a ship there!")
+						print("You already have a ship there!", Map[row][column])
 					else:
 						go2 = 1
 			opt = 1
 			options = ["","","","","",""]
 			print("Please choose direction:")
-			if int(Coordinates[1])-int(ShipYard[1][i]) > 0 and Overlap(row,column,ShipYard[1][i],Map,'U') == False:
+			if int(row)-int(cShipYard[1][i]) > 0 and Overlap(row,column,cShipYard[1][i],Map,'U') == False:
 				print(opt, ": Up")
 				options[opt] = "Up"
 				opt = opt + 1
-			if int(Coordinates[1])+int(ShipYard[1][i]) < 11 and Overlap(row,column,ShipYard[1][i],Map,'D') == False:
+			if int(row)+int(cShipYard[1][i]) < 11 and Overlap(row,column,cShipYard[1][i],Map,'D') == False:
 				print(opt, ": Down")
 				options[opt] = "Down"
 				opt = opt + 1
-			if int(Coordinates[0])-int(ShipYard[1][i]) > 0 and Overlap(row,column,ShipYard[1][i],Map,'L') == False:
+			if int(column)-int(cShipYard[1][i]) > 0 and Overlap(row,column,cShipYard[1][i],Map,'L') == False:
 				print(opt, ": Left")
 				options[opt] = "Left"
 				opt = opt + 1
-			if int(Coordinates[0])+int(ShipYard[1][i]) < 11 and Overlap(row,column,ShipYard[1][i],Map,'R') == False:
+			if int(column)+int(cShipYard[1][i]) < 11 and Overlap(row,column,cShipYard[1][i],Map,'R') == False:
 				print(opt, ": Right")
 				options[opt] = "Right"
 				opt = opt + 1
@@ -66,24 +71,25 @@ def FillBoard():
 			if Direction > opt or Direction < 1:
 				print("Please choose a listed option")
 			elif options[Direction] == "Up":
-				for j in range (0,ShipYard[1][i]):
-					Map[row - j][column] = ShipYard[0][i] + " "
+				for j in range (0,cShipYard[1][i]):
+					Map[row - j][column] = cShipYard[0][i] + " "
 					go = 1
 			elif options[Direction] == "Down":
-				for j in range (0,ShipYard[1][i]):
-					Map[row + j][column] = ShipYard[0][i] + " "
+				for j in range (0,cShipYard[1][i]):
+					Map[row + j][column] = cShipYard[0][i] + " "
 					go = 1
 			elif options[Direction] == "Left":
-				for j in range (0,ShipYard[1][i]):
-					Map[row][column - j] = ShipYard[0][i] + " "
+				for j in range (0,cShipYard[1][i]):
+					Map[row][column - j] = cShipYard[0][i] + " "
 					go = 1
 			elif options[Direction] == "Right":
-				for j in range (0,ShipYard[1][i]):
-					Map[row][column + j] = ShipYard[0][i] + " "
+				for j in range (0,cShipYard[1][i]):
+					Map[row][column + j] = cShipYard[0][i] + " "
 					go = 1
 			else:
 				go = 0
 		Display(Map)
+	return Map
 
 def Overlap(row,column,Length,Map,Direction):
 	if Direction == 'U':
@@ -150,8 +156,6 @@ def ConvertCoordinates(Point):
 			print("Coordinates should consist of only 1 letter and 1 number.")
 			row = 0
 			column = 0
-	if column == 0:
-		column = 10
 	Coordinates = [row,column]
 	return Coordinates	
 
@@ -169,6 +173,224 @@ def Display(Map):
 			print (Map[i][j],end='')
 		print()
 
-Board1 = FillBoard()
+def LaunchAttack(Row,Col,Map):
 
-Display(Board1)
+	Target = Map[Row,Col]
+	if Target == "- ":
+		Damage = ["o ","False",]
+	elif Target == "A ":
+		Damage = ["X ","True", 0]
+	elif Target == "B ":
+		Damage = ["X ", "True", 1]
+	elif Target == "C ":
+		Damage = ["X ", "True", 2]
+	elif Target == "D ":
+		Damage = ["X ", "True", 3]
+	elif Target == "P ":
+		Damage = ["X ", "True", 4]
+	return Damage
+
+
+def FriendMode():
+
+	os.system("cls")
+
+	Player = 1
+	winner = False
+	ShipYard1 = ShipYard2 = cShipYard
+	score1 = score2 = 17
+
+	print("Player 1: Place your ships.")
+	Board1 = FillBoard()
+
+	os.system("cls")
+
+	print("Player 2: Place your ships")
+	Board2 = FillBoard()
+
+	os.system("cls")
+
+
+	Radar1 = BlankMap
+	Radar2 = BlankMap
+
+	while winner == False:
+
+		if Player == 1:
+			print("--------------------")
+			print("      PLAYER 1      ")
+			print("--------------------")
+			print()
+			input("Press Enter to reveal board")
+			os.system("cls")
+			print("--------------------")
+			print("      PLAYER 1      ")
+			print("--------------------")
+			print()
+			print("RADAR")
+			Display(Radar1)
+			print()
+			print()
+			print("YOUR SHIPS")
+			Display(Board1)
+			print()
+			Attack = input("Input Attack Coordinates: ")
+
+			AttackCoord = ConvertCoordinates(Attack)
+
+			AtRow = int(AttackCoord[0])
+			AtCol = int(AttackCoord[1])
+			if AtCol == 0:
+				AtCol = 10
+
+			Results = LaunchAttack(AtRow,AtCol,Board2)
+
+			Radar1[AtRow][AtCol] = Results[0]
+
+			os.system("cls")
+			print("--------------------")
+			print("      PLAYER 1      ")
+			print("--------------------")
+			print()
+			print("RADAR")
+			Display(Radar1)
+			print()
+			print()
+			print("YOUR SHIPS")
+			Display(Board1)
+			print()
+
+			if Results[1] == "True":
+				ShipYard1[1][Results[2]] = ShipYard1[1][Results[2]] - 1
+				if ShipYard1[1][Results[2]] == 0:
+					print("You sank their ", ShipYard[2][Results[2]], "!!")
+				else:
+					print("Hit!!")
+				score2 = score2 - 1
+			else:
+				print("Miss")
+			print()
+			print()
+
+			input("Press Enter to end turn")
+			print()
+			print()
+
+			input("Press Enter to end turn")
+		else:
+			print("--------------------")
+			print("      PLAYER 2      ")
+			print("--------------------")
+			print()
+			input("Press Enter to reveal board")
+			os.system("cls")
+			print("--------------------")
+			print("      PLAYER 2      ")
+			print("--------------------")
+			print()
+			print("RADAR")
+			Display(Radar2)
+			print()
+			print()
+			print("YOUR SHIPS")
+			Display(Board2)
+			print()
+			Attack = input("Input Attack Coordinates: ")
+
+			AttackCoord = ConvertCoordinates(Attack)
+
+			AtRow = int(AttackCoord[0])
+			AtCol = int(AttackCoord[1])
+			if AtCol == 0:
+				AtCol = 10
+
+			Results = LaunchAttack(AtRow,AtCol,Board1)
+
+			Radar2[AtRow][AtCol] = Results[0]
+
+			os.system("cls")
+			print("--------------------")
+			print("      PLAYER 2      ")
+			print("--------------------")
+			print()
+			print("RADAR")
+			Display(Radar2)
+			print()
+			print()
+			print("YOUR SHIPS")
+			Display(Board2)
+			print()
+
+			if Results[1] == "True":
+				ShipYard1[1][Results[2]] = ShipYard1[1][Results[2]] - 1
+				if ShipYard1[1][Results[2]] == 0:
+					print("You sank their ", ShipYard[2][Results[2]], "!!")
+				else:
+					print("Hit!!")
+				Score1 = Score1 - 1
+			else:
+				print("Miss")
+			print()
+			print()
+			input("Press Enter to end turn")
+
+		if score1 == 0 or score2 == 0:
+			winner = 1
+		else:
+			if player == 1:
+				player = 2
+			else:
+				player = 1
+
+	print("Player ", player, " wins!!")
+
+
+
+
+
+
+print("                                     |__")
+print("                                     |\/")
+print("                                     ---")
+print("                                     / | [")
+print("                              !      | |||")
+print("                            _/|     _/|-++'")
+print("                        +  +--|    |--|--|_ |-")
+print("                     { /|__|  |/\__|  |--- |||__/")
+print("                    +---------------___[}-_===_.'____                 /\ ")
+print("                ____`-' ||___-{]_| _[}-  |     |_[___\==--            \/   _")
+print(" __..._____--==/___]_|__|_____________________________[___\==--____,------' .7")
+print("|                                                                     BB-61/")
+print(" \_________________________________________________________________________|")
+print()
+print()
+print("              _           _   _   _           _     _       ")
+print("             | |         | | | | | |         | |   (_)      ")
+print("             | |__   __ _| |_| |_| | ___  ___| |__  _ _ __  ")
+print("             | '_ \ / _` | __| __| |/ _ \/ __| '_ \| | '_ \ ")
+print("             | |_) | (_| | |_| |_| |  __/\__ \ | | | | |_) |")
+print("             |_.__/ \__,_|\__|\__|_|\___||___/_| |_|_| .__/ ")
+print("                                                     | |    ")
+print("                                                     |_|    ")
+print()
+print()
+
+Exit = False
+while Exit == False:
+	print("Choose a game mode:")
+	print("1: Player vs Player")
+	print("2: Player vs AI")
+	print("0: Exit")
+	print()
+	mode = int(input("Choice:   "))
+
+	if mode == 1:
+		FriendMode()
+	elif mode == 2:
+		print("Still in development.")
+	elif mode == 0:
+		print ("Goodbye!!")
+		Exit = True
+	else:
+		print("Please choose a valid mode")
+		print(mode)
